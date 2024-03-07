5165ee0a0868c2d786476fa3a19ae5df83383396
tamarabolmanac <tamarabolmanac@gmail.com>
2024-03-07 20:41:46 +0100

    Rescue parsing and saving; Log the error

--- app/services/emploies_service.rb
+++ app/services/emploies_service.rb
@@ -22,7 +22,11 @@ class EmploiesService
   def create_and_store_employee_data(employee_data)
     begin
       employee = create_new_employee(employee_data)
-      employee.save! if employee.valid?
+      if employee.valid?
+        employee.save
+      else
+        Rails.logger.error("Validation failed for employee: #{employee.errors.full_messages}. Employee data: #{employee_data}")
+      end
     rescue => e
       Rails.logger.error("Error creating employee: #{e.message}. Employee data: #{employee_data}")
     end
@@ -31,21 +35,14 @@ class EmploiesService
   def create_new_employee(employee_data)
     Employee.create(
       external_id: employee_data["id"],
-      date_of_birth: parse_date(employee_data["date_of_birth"]),
+      date_of_birth: begin Date.parse(employee_data["date_of_birth"]) rescue nil end,
       first_name: employee_data["first_name"],
       last_name: employee_data["last_name"],
       email: employee_data["email"],
       address: employee_data["address"],
       country: employee_data["country"],
-      bio: employee_data["bio"].join(' '),
+      bio: employee_data["bio"]&.join(' '),
       rating: employee_data["rating"]
     )
-  end
-
-  def parse_date(date_string)
-    Date.parse(date_string)
-  rescue ArgumentError => e
-    Rails.logger.error("Invalid date format: #{date_string}. Error: #{e.message}")
-    nil
   end
 end
